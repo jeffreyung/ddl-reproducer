bb2a5148c3359770f1d3e1a06f2640c94483e959
jeffreyung <jeffrey.ung97@gmail.com>
2020-12-12 20:05:32 +1100

    added json to array

--- big-query/bq-ddl-reproduce.sh
+++ big-query/bq-ddl-reproduce.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 #
-# Retrieves the DDL of a table 
+# retrieves the DDL of a table.
 
+# prints the usage message if the provided argument or flag is invalid.
 print_usage() {
 	echo "Usage: $0 <project_id> <dataset> <table>"
 }
 
+# checks if the required arguments are provided.
 if [[ $1 == "" || $2 == "" || $3 == "" ]]; then
 	print_usage
 	exit 0
@@ -20,5 +22,10 @@ fi
 #       	esac
 #done
 
-TABLE_INFO=$(bq show --format=prettyjson $1:$2.$3)
-echo $TABLE_INFO
+table_info=$(bq show --format=prettyjson $1:$2.$3)
+col_names=( $(echo $table_info | jq '.schema.fields[].name') )
+col_types=( $(echo $table_info | jq '.schema.fields[].type') )
+col_modes=( $(echo $table_info | jq '.schema.fields[].mode') )    
+printf '%s\n' "${col_names[@]}"
+printf '%s\n' "${col_types[@]}"
+printf '%s\n' "${col_modes[@]}"
