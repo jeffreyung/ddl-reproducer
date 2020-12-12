194f3f263ce396b1c7b9d36657de0e12f22d2286
jeffreyung <jeffrey.ung97@gmail.com>
2020-12-13 00:51:55 +1100

    added support for partitioning and cluster

--- big-query/bq-ddl-reproduce.sh
+++ big-query/bq-ddl-reproduce.sh
@@ -30,6 +30,7 @@ table_info=$(bq show --format=prettyjson $1:$2.$3)
 # The table type can be either a 'table' or 'view'.
 table_type=$(echo "$table_info" | jq '.type')
 
+# Formatting string to create the valid DDL..
 if [[ $table_type == "\"TABLE\"" ]]
 then
 	col_names=( $(echo "$table_info" | jq '.schema.fields[].name') )
@@ -37,24 +38,47 @@ then
 	col_modes=( $(echo "$table_info" | jq '.schema.fields[].mode') )
 	partitioning_field=$(echo "$table_info" | jq '.timePartitioning.field')
 	partitioning_type=$(echo "$table_info" | jq '.timePartitioning.type')  
-	clustering_fields=( $(echo "$table_info" | jq '.clustering.fields[]') )
+	clustering_fields=( $(echo "$table_info" | jq '.clustering.fields[]') ) 
 
-	# Formatting string to a valid create statement (this will be messy).
 	cols=()
-	for i in "${!col_names[@]}"; do
+	partition_idx=0
+	actual_partition_type="DATE"
+	for i in "${!col_names[@]}"
+	do
 		cols+="${col_names[$i]}-$(echo "${col_types[$i]}" | sed "s/INTEGER/INT64/" \
 			| sed "s/FLOAT/FLOAT64/" | sed "s/BOOLEAN/BOOL/") "
+
+		if [[ "${col_names[$i]}" == "$partitioning_field" ]]
+		then
+			partitioning_idx=$i
+			actual_partition_type=${col_types[$i]}
+		fi
 	done
 
 	joined_cols=$(join ", " ${cols[@]})
 	joined_cols=${joined_cols//\"/}
 	joined_cols=${joined_cols//\-/ }
-	joined_clusters=$(join ", " $clustering_fields)
+	joined_clusters=$(join ", " ${clustering_fields[@]})
 	joined_clusters=${joined_clusters//\"/}
 
 	# Outputs the create statement for the table.
-	echo "CREATE OR REPLACE TABLE \`$project.$dataset.$table\` ($joined_cols) \
-		CLUSTER BY $joined_clusters;"
+	printf "CREATE OR REPLACE TABLE \`$project.$dataset.$table\` ($joined_cols)"
+	partitioning_field=${partitioning_field//\"/}
+	if [[ "$partitioning_field" != "null" ]]
+	then
+		if [[ "$actual_partition_type" == "\"DATE\"" || \
+			"$actual_partition_type" == "DATE" ]]
+		then
+			printf "\nPARTITION BY "$partitioning_field""
+		else
+			printf "\nPARTITION BY DATE("$partitioning_field")"
+		fi
+	fi
+	if [[ "$joined_clusters" != "" ]]
+	then
+		printf "\nCLUSTER BY $joined_clusters"
+	fi
+	printf ";\n"
 elif [[ $table_type == "\"VIEW\"" ]]
 then
 	query=$(echo "$table_info" | jq '.view.query')
